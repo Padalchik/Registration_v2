8fdfd02de2a8309f3e11e47e9d819c6af542fe36
Padalchik <andrey_nikiforov_1998@mail.ru>
2025-03-09 12:57:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Registration_v2;
+using Registration_v2.Entity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DataBaseConnectionString"));
+});
+
+builder.Services.AddDefaultIdentity<CustomUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDBContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.MapGet("/", () => Results.Redirect("/Identity/Account/Register"));
+app.MapRazorPages();
 
 app.Run();
